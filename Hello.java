9ae984093b3d4d40306d6708fd708fdfce01105f
kduong@stuy.edu <kduong@stuy.edu>
2018-09-16 17:39:20 -0400

    Added working code, also really hope someone gets this basketball reference

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public class Hello{
+  public static void main (String[] args) {
+    System.out.println("Hello my name is Kaitlyn, KD like the snake for short but no one has ever called me that.");
+  }
+}
